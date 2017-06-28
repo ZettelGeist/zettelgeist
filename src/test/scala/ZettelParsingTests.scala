d0af637e5e9c7841f445dd6778a8732d3e654be1
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-06-27 22:09:41 -0500

    add all should be assertions for all tests

--- src/test/scala/ZettelParsingTests.scala
+++ src/test/scala/ZettelParsingTests.scala
@@ -34,7 +34,14 @@ class ZettelParsingTests extends FlatSpec with Matchers {
     val zettel = jsonAst.extract[Zettel]
 
     zettel.title.get should be ("My First Zettel")
+    zettel.mentions.get should be (List("dbdennis","gkt"))
+    zettel.tags.get should be (List("Charles Babbage", "Ada Lovelace"))
+    zettel.cite.get should be (Citation("Ifrah", Some(22), Some(36)))
     zettel.dates.get should be (Dates(1841, None, Some("CE")))
+    zettel.summary.get should be ("An amazing Zettel")
+    zettel.text.get should be ("Text of Zettel")
+    zettel.bibkey.get should be ("BibKey")
+    zettel.bibtex.get should be ("@article{key, entries}")
   }
 
   it should "parse with no fields present in JSON zettel" in {
@@ -46,5 +53,41 @@ class ZettelParsingTests extends FlatSpec with Matchers {
     val zettel = json.extract[Zettel]
 
     zettel.title should be (None)
+    zettel.mentions should be (None)
+    zettel.tags should be (None)
+    zettel.cite should be (None)
+    zettel.dates should be (None)
+    zettel.summary should be (None)
+    zettel.text should be (None)
+    zettel.bibkey should be (None)
+    zettel.bibtex should be (None)
+  }
+
+  it should "parse with some fields present in JSON zettel" in {
+    val zettelInJson = """{
+      "title" : "My First Zettel",
+      "mentions" : [ "dbdennis", "gkt"],
+      "cite" : {
+        "bibkey" : "Ifrah",
+        "page": 22,
+        "last_page": 36
+      }
+     }
+    """
+
+    implicit val formats = DefaultFormats // Brings in default date formats etc.
+
+    val json = parse(zettelInJson)
+    val zettel = json.extract[Zettel]
+
+    zettel.title.get should be ("My First Zettel")
+    zettel.mentions.get should be (List("dbdennis","gkt"))
+    zettel.cite.get should be (Citation("Ifrah", Some(22), Some(36)))
+    zettel.tags should be (None)
+    zettel.dates should be (None)
+    zettel.summary should be (None)
+    zettel.text should be (None)
+    zettel.bibkey should be (None)
+    zettel.bibtex should be (None)
   }
 }
