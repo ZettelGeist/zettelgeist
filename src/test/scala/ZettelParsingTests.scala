fb8530806db766bdb05453e3a839d55dd97e0b1e
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-06-30 17:53:05 -0500

    using circe-yaml to load a Zettel and bind it to a case class -- it works!!

--- src/test/scala/ZettelParsingTests.scala
+++ src/test/scala/ZettelParsingTests.scala
@@ -4,13 +4,19 @@
 
 package zettelgeist
 
+import io.circe.Error
 import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods._
 import org.scalatest._
+import cats.syntax.either._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.yaml
 
 class ZettelParsingTests extends FlatSpec with Matchers {
   "Zettel" should "parse with all fields present in JSON zettel" in {
-    val zettelInJSON = """{
+    val zettelInJSON =
+      """{
       |"title" : "My First Zettel",
       |"mentions" : [ "dbdennis", "gkt"],
       |"tags" : ["Charles Babbage", "Ada Lovelace"],
@@ -63,7 +69,8 @@ class ZettelParsingTests extends FlatSpec with Matchers {
   }
 
   it should "parse with some fields present in JSON zettel" in {
-    val zettelInJson = """{
+    val zettelInJson =
+      """{
       |"title" : "My First Zettel",
       |"mentions" : [ "dbdennis", "gkt"],
       |"cite" : {
@@ -72,7 +79,6 @@ class ZettelParsingTests extends FlatSpec with Matchers {
       |  "last_page": 36
       |}
       |}""".stripMargin
-
     implicit val formats = DefaultFormats // Brings in default date formats etc.
 
     val json = parse(zettelInJson)
@@ -91,15 +97,11 @@ class ZettelParsingTests extends FlatSpec with Matchers {
 
   "Circe YAML" should "run this slightly-modified example from their own docs" in {
 
-    import cats.syntax.either._
-    import io.circe._
-    import io.circe.generic.auto._
-    import io.circe.yaml
-
     case class Nested(one: String, two: BigDecimal)
     case class Foo(foo: String, bar: Nested, baz: List[String], extra1: Option[String], extra2: Option[String])
 
-    val json = yaml.parser.parse("""
+    val json = yaml.parser.parse(
+      """
        |foo: Hello, World
        |bar:
        |  one: One Third
@@ -107,7 +109,9 @@ class ZettelParsingTests extends FlatSpec with Matchers {
        |baz:
        |- Hello
        |- World
-       |extra2: extra2""".stripMargin)
+       |extra2: extra2""".
+      stripMargin
+    )
 
     val foo = json
       .leftMap(err => err: Error)
@@ -119,5 +123,46 @@ class ZettelParsingTests extends FlatSpec with Matchers {
     foo.baz should be(List("Hello", "World"))
     foo.extra1 should be(None)
     foo.extra2 should be(Some("extra2"))
+  }
+
+  it should "be able to load a Zettel in YAML format and create case class" in {
+    val json = yaml.parser.parse(
+      """
+       |title: My First Zettel
+       |mentions:
+       |- dbdennis
+       |- gkt
+       |tags:
+       |- Charles Babbage
+       |- Ada Lovelace
+       |cite:
+       |  bibkey: Ifrah
+       |  page: 22
+       |  last_page: 36
+       |dates:
+       |  year: 1841
+       |  era: CE
+       |summary: An amazing Zettel
+       |text: Text of Zettel
+       |bibkey: BibKey
+       |bibtex: "@article{key, entries}"
+       |""".stripMargin
+    )
+
+    val zettel =
+      json
+        .leftMap(err => err: Error)
+        .flatMap(_.as[Zettel])
+        .valueOr(throw _)
+
+    zettel.title.get should be("My First Zettel")
+    zettel.mentions.get should be(List("dbdennis", "gkt"))
+    zettel.tags.get should be(List("Charles Babbage", "Ada Lovelace"))
+    zettel.cite.get should be(Citation("Ifrah", Some(22), Some(36)))
+    zettel.dates.get should be(Dates(1841, None, Some("CE")))
+    zettel.summary.get should be("An amazing Zettel")
+    zettel.text.get should be("Text of Zettel")
+    zettel.bibkey.get should be("BibKey")
+    zettel.bibtex.get should be("@article{key, entries}")
   }
 }
