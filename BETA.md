f9e3c4438686a95aeecfde85789a9593b3d7238b
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-07-17 13:38:01 -0500

    refactoring zimport

---
+++ BETA.md
@@ -0,0 +1,21 @@
+These are some quick notes for the beta.
+
+Beginning July 17, 2017, we will be placing emphasis on packaging and being able to "use" the tools, even as they are
+being created.
+
+Before beginnign a session, you should set up your environment variables. This is done by sourcing the `setpath.sh` 
+script as follows:
+
+```shell
+cd zettelgeist
+source setpath.sh
+```
+
+You can also put this in your `.bashrc` (or equivalent, e.g. .zshrc) if you wish to avoid re-running it each time you 
+start a new terminal session, using your favorite text editor.
+
+Once you have made these settings, you can run any of the ZettelGeist commands, e.g. `zcreate`, `zimport`, and the like.
+Not all commands are available yet but will be soon.
+
+To find out what commands are available, you can `ls zettelgeist/bin` to see what commands are available.
+
--- zettelgeist/zimport.py
+++ zettelgeist/zimport.py
@@ -4,59 +4,62 @@ import os
 import os.path
 import sys
 import yaml
-import zdb
+from zettelgeist import zdb
 
-parser = zdb.get_argparse()
-parser.add_argument('--zettel-dir', help="location of Zettels (path to folder)")
-parser.add_argument('--validate', action="store_true", help="check Zettels only (don't import)", default=False)
+def main():
+  parser = zdb.get_argparse()
+  parser.add_argument('--zettel-dir', help="location of Zettels (path to folder)")
+  parser.add_argument('--validate', action="store_true", help="check Zettels only (don't import)", default=False)
 
-args = parser.parse_args()
-dir = args.zettel_dir
-if not dir:
-   parser.print_help()
-   sys.exit(1)
+  args = parser.parse_args()
+  dir = args.zettel_dir
+  if not dir:
+     parser.print_help()
+     sys.exit(1)
 
-db = zdb.get(args.database)
+  db = zdb.get(args.database)
 
-for filename in os.listdir(dir):
-  if not filename.endswith('.yaml'):
-    print("Ignoring %s; add .yaml extension to import this file." % filename)
-    continue
-  print("Importing %s" % filename)
-  filepath = os.path.join(dir, filename)
-  with open(filepath) as infile:
-    try:
-       text = infile.read()
-    except:
-       print("- I/O error on %s: Encoding must be UTF-8" % filename)
-       continue
-    try:
-       ydocs = yaml.load_all(text)
-    except:
-       print("- YAML load failure (run yamllint on this file)")
-       continue
-
-    try:
-      ydocs = list(ydocs)
-    except:
-      print("- YAML loaded but could not be processed")
+  for filename in os.listdir(dir):
+    if not filename.endswith('.yaml'):
+      print("Ignoring %s; add .yaml extension to import this file." % filename)
       continue
+    print("Importing %s" % filename)
+    filepath = os.path.join(dir, filename)
+    with open(filepath) as infile:
+      try:
+         text = infile.read()
+      except:
+         print("- I/O error on %s: Encoding must be UTF-8" % filename)
+         continue
+      try:
+         ydocs = yaml.load_all(text)
+      except:
+         print("- YAML load failure (run yamllint on this file)")
+         continue
 
-    ydoc_id = 0
-    for ydoc in ydocs:
-      if type(ydoc) == type({}):
-        ydoc['filename'] = filename
-        if args.validate:
-          errors = db.check(ydoc)
-          if len(errors) > 0:
-            if len(ydocs) > 0:
-              print("-- The following messages are for Fass entry %d" % ydoc_id)
-            for error in errors:
-              print("-- " + error)
-        else:
-          db.bind(ydoc)
-          db.insert_into_table()
-      ydoc_id = ydoc_id + 1
+      try:
+        ydocs = list(ydocs)
+      except:
+        print("- YAML loaded but could not be processed")
+        continue
 
-db.done()
+      ydoc_id = 0
+      for ydoc in ydocs:
+        if type(ydoc) == type({}):
+          ydoc['filename'] = filename
+          if args.validate:
+            errors = db.check(ydoc)
+            if len(errors) > 0:
+              if len(ydocs) > 0:
+                print("-- The following messages are for Fass entry %d" % ydoc_id)
+              for error in errors:
+                print("-- " + error)
+          else:
+            db.bind(ydoc)
+            db.insert_into_table()
+        ydoc_id = ydoc_id + 1
 
+  db.done()
+
+if __name__ == '__main__':
+  main()
