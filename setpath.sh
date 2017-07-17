58624652a579777023aabebcbd72020a3a862dfd
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-07-17 08:51:28 -0500

    add script for setting PATH/PYTHONPATH for users who check the tools out

---
+++ setpath.sh
@@ -0,0 +1,11 @@
+#
+# setpath.sh: A script you can source to set the PATH and PYTHONPATH to find the ZettelGeist tools.
+#
+# Usage:
+# $ source setpath.sh
+#
+
+# TODO: Make this is a bit smarter later.
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+export PATH=$(pwd)/bin:$PATH
