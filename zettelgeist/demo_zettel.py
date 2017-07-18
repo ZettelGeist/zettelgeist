4eb70f36173f664544f14b182945afed9b197fe6
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-07-17 23:09:54 -0500

    removing files no longer needed

--- zettelgeist/demo_zettel.py
+++
@@ -1,9 +0,0 @@
-from zettel import Zettel
-
-z = Zettel()
-z.set_field('title', 'Title')
-z.set_field('tags', "tag1")
-z.append_field('tags', "tag2")
-z.append_field('tags', "tag3")
-z.load_field('text', 'ps.text')
-print(z.get_yaml())
