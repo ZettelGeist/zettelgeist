5c489829d54459009274859c358a687c15a71881
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-12-19 14:04:32 -0600

    last beta release

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 setup(
     name='zettelgeist',
     packages=['zettelgeist'],
-    version='0.9',
+    version='0.9.2',
     description='ZettelGeist - a historiographically focused notetaking system',
     author='ZettelGeist Laboratories',
     author_email='gkt@cs.luc.edu',
     license='Apache License 2.0',
     url='https://github.com/zettelgeist/zettelgeist',
-    download_url='https://github.com/zettelgeist/zegttelgeist/archive/0.9.tar.gz',
+    #download_url='https://github.com/zettelgeist/zegttelgeist/archive/0.9.tar.gz',
     keywords=['notetaking', 'YAML', 'Markdown', 'sqlite3', 'GitHub'],
     classifiers=[
         'Development Status :: 4 - Beta',
