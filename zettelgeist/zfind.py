f4b23ad738b1bfcee9bd5fd0559c42a132b213cb
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-02-10 22:17:27 -0600

    restore ZQL support in zfind.py - eliminate naive --find-* options (fixes #4)

--- zettelgeist/zfind.py
+++ zettelgeist/zfind.py
@@ -1,65 +1,75 @@
+import sys
 import argparse
-from zettelgeist import zdb, zettel
+from zettelgeist import zdb, zettel, zquery
 
 
 def get_argparse():
     parser = zdb.get_argparse()
-    parser.add_argument('--text', action='store_const',
+    parser.add_argument('--use-zettel', action='store_const',
                         const=True, default=False)
 
     for field in zdb.ZettelSQLFields:
-        parser.add_argument('--find-%s' %
-                            field, help='search the Zettel %s field' % field)
-        parser.add_argument('--exclude-%s' %
-                            field, help='search the Zettel %s field' % field)
         parser.add_argument('--show-%s' % field,
-                            action='store_const', const=True, default=False)
+                            action='store_const', const=True, default=False,
+                            help="include field <%s> in output" % field)
 
     parser.add_argument('--count', action='store_const', const=True,
                         default=False, help="Show number of Zettels matching this search")
+    parser.add_argument('--query-prompt', action='store_const', const=True,
+                        default=False, help="enter query interactively (overrides --input))")
+
+    parser.add_argument(
+        '--query-file', help="load query from file", default=None)
+
+    parser.add_argument(
+        '--query', help="query from command line (use single quotes)", default=None)
     return parser
 
 
 def main():
     parser = get_argparse()
     args = parser.parse_args()
-
     argsd = vars(args)
-    query = []
-    for field in zdb.ZettelSQLFields:
-        exclude_field = 'exclude_' + field
-        include_field = 'find_' + field
-        if exclude_field in argsd:
-            entry = argsd.get(exclude_field)
-            if entry:
-                query.append((field, '-', entry))
-        if include_field in argsd:
-            entry = argsd.get(include_field)
-            if entry:
-                query.append((field, '', entry))
 
-    db = zdb.get(args.database)
-    gen = db.fts_search(query)
+    if args.query_prompt:
+        input_line = input("zfind:zql> ")
+    elif args.query_file:
+        with open(args.query_file) as infile:
+            input_line = infile.read()
+    elif args.query:
+        input_line = args.query
+    else:
+        input_line = None
+
+    if input_line:
+        ast = zquery.compile(input_line)
+        db = zdb.get(args.database)
+        gen = db.fts_query(ast[0])
+    else:
+        print("Warning: Query could not be loaded via --query, --query-file, or --query-prompt")
+        sys.exit(1)
 
     search_count = 0
     for row in gen:
         search_count = search_count + 1
         printed_something = False
-        loader = zettel.ZettelLoader(row['filename'])
-        zettels = loader.getZettels()
-        z = next(zettels)
-        if z == None:
-            print("Could not load source Zettel: %s" % row['filename'])
-            sys.exit(1)
+        if args.use_zettel:
+            loader = zettel.ZettelLoader(row['filename'])
+            zettels = loader.getZettels()
+            z = next(zettels)
+        else:
+            z = None
 
         for field in row.keys():
             show_field = "show_" + field
             if argsd.get(show_field, None):
                 if row[field]:
-                    if args.text:
-                        print(z.get_text([field]))
-                    else:
+                    if z and field in zettel.ZettelFields:
                         print(z.get_yaml([field]))
+                    else:
+                        print("%s:" % field)
+                        print(row[field])
+                        print()
                     printed_something = True
 
         if printed_something:
--- zettelgeist/zversion.py
+++ zettelgeist/zversion.py
@@ -2,7 +2,7 @@
 # ZettelGeist Version for Python
 #
 
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 def version():
     return __version__
